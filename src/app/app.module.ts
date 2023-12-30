60e6a9c17332602927845295905d61e27b595a06
doubleD <ddwork200698@gmail.com>
2023-12-30 21:31:36 +0700

    Tach detail san pham

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { OrderComponent } from './order/order.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 @NgModule({
-  declarations: [HomeComponent, HeaderComponent, FooterComponent],
+  declarations: [HomeComponent, HeaderComponent, FooterComponent, OrderComponent, ProductDetailComponent],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [HomeComponent]
+  bootstrap: [
+    HomeComponent,
+    //OrderComponent
+  ]
 })
 export class AppModule { }
